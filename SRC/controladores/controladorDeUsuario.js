8a2571a08c30fa5036be23c90d296f60a4c85b9a
Whybson Jackson De Assis <115576665+JackobAssis@users.noreply.github.com>
2023-11-08 18:49:02 -0300

    Update controladorDeUsuario.js
    Signed-off-by: Whybson Jackson De Assis <115576665+JackobAssis@users.noreply.github.com>

--- SRC/controladores/controladorDeUsuario.js
+++ SRC/controladores/controladorDeUsuario.js
@@ -2,13 +2,13 @@ const usuarios = [];
 
 
 const cadastrarUsuario = (req, res) => {
-    const { nome, email, senha } = req.body;
+const { nome, email, senha } = req.body;
 
     if (!nome || !email || !senha) {
         return res.status(400).json({ mensagem: 'Nome, email e senha são obrigatórios' });
     }
 
-    const novoUsuario = {
+const novoUsuario = {
         nome,
         email,
         senha,
@@ -21,13 +21,13 @@ const cadastrarUsuario = (req, res) => {
 
 
 const fazerLogin = (req, res) => {
-    const { email, senha } = req.body;
+const { email, senha } = req.body;
 
     if (!email || !senha) {
         return res.status(400).json({ mensagem: 'Email e senha são obrigatórios' });
     }
 
-    const usuario = usuarios.find((user) => user.email === email && user.senha === senha);
+const usuario = usuarios.find((user) => user.email === email && user.senha === senha);
 
     if (!usuario) {
         return res.status(401).json({ mensagem: 'Credenciais inválidas' });
@@ -40,14 +40,14 @@ const fazerLogin = (req, res) => {
 
 const detalharPerfil = (req, res) => {
 
-    const usuarioLogado = req.usuario;
+const usuarioLogado = req.usuario;
 
     return res.status(200).json(usuarioLogado);
 };
 
 const editarPerfil = (req, res) => {
-    const { nome, email } = req.body;
-    const usuarioLogado = req.usuario;
+const { nome, email } = req.body;
+const usuarioLogado = req.usuario;
 
     if (nome) {
         usuarioLogado.nome = nome;
